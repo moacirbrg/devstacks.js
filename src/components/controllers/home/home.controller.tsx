d9806eeab26035125db04e0496c8937bdbad0ec2
Moacir Braga <contato@moacirbrg.org>
2023-12-24 17:06:35 -0300

    Added body, primary and secondary colors to the color schema

--- src/components/controllers/home/home.controller.tsx
+++ src/components/controllers/home/home.controller.tsx
@@ -1,15 +1,12 @@
 import { FrameworkElement } from '@libs/core/framework-element';
 import { Component } from '@libs/core/component';
 import { DOM } from '@libs/core/dom';
-import { TypoMessageShared } from '@libs/ui/typo/message/typo-message.shared';
-import { TypoMessageColor } from '@libs/ui/typo/message/typo-message.enum';
 
 export class HomeController extends Component {
     public render(): FrameworkElement {
         return (
             <div>
                 <div>Home</div>
-                <TypoMessageShared Color={TypoMessageColor.Info}>My tiny message!</TypoMessageShared>
             </div>
         );
     }
