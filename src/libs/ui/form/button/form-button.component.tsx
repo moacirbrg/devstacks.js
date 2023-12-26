aff07e017f75a60cd83ce9b75d7e2a60ce5f4a77
Moacir Braga <contato@moacirbrg.org>
2023-12-26 20:39:03 -0300

    FormButton partially implemented

---
+++ src/libs/ui/form/button/form-button.component.tsx
@@ -0,0 +1,59 @@
+import { FrameworkElement } from '@libs/core/framework-element';
+import { Component } from '@libs/core/component';
+import { ComponentProps } from '@libs/core/component-props';
+import { Color } from "@libs/ui/enums/color.enum";
+import { FontFamily } from '@libs/ui/enums/font-family.enum';
+import { FontWeight } from '@libs/ui/enums/font-weight.enum';
+import { DOM } from '@libs/core/dom';
+import { Styles } from '@libs/ui/styles';
+import { FormButtonGlobals } from '@libs/ui/form/button/form-button.globals';
+import { FontContentSize } from '@libs/ui/enums/font-content-size.enum';
+
+export interface FormButtonProps extends ComponentProps {
+    Color?: Color;
+    FontFamily?: FontFamily;
+    FontSize?: FontContentSize;
+    FontWeight?: FontWeight;
+}
+
+export class FormButton extends Component<FormButtonProps> {
+    public render(): FrameworkElement {
+        const element: FrameworkElement = (
+            <div class="form-button-component">
+                {this.children}
+            </div>
+        );
+
+        Styles.setDataColor(element, this.props.Color || FormButtonGlobals.Color);
+        Styles.setFontFamily(element, this.props.FontFamily || FormButtonGlobals.FontFamily);
+        Styles.setFontSize(element, this.props.FontSize || FormButtonGlobals.FontSize);
+        Styles.setFontWeight(element, this.props.FontWeight || FormButtonGlobals.FontWeight);
+
+        return element;
+    }
+
+    public setColor(color: Color): void {
+        this.props.Color = color;
+        Styles.setDataColor(this.element!, color);
+    }
+
+    public setFontFamily(fontFamily: FontFamily): void {
+        this.props.FontFamily = fontFamily;
+        Styles.setFontFamily(this.element!, fontFamily);
+    }
+
+    public setFontSize(fontSize: FontContentSize): void {
+        this.props.FontSize = fontSize;
+        Styles.setFontSize(this.element!, fontSize);
+    }
+
+    public setFontWeight(fontWeight: FontWeight): void {
+        this.props.FontWeight = fontWeight;
+        Styles.setFontWeight(this.element!, fontWeight);
+    }
+
+    public setContent(content: string|FrameworkElement): void {
+        this.children = [content];
+        DOM.replaceChildren(this.element!, ...this.children as string[]|FrameworkElement[]);
+    }
+}
--- src/libs/ui/styles.ts
+++ src/libs/ui/styles.ts
@@ -3,8 +3,13 @@ import { FrameworkElement } from '@libs/core/framework-element';
 import { FontWeight } from '@libs/ui/enums/font-weight.enum';
 import { FontContentSize } from '@libs/ui/enums/font-content-size.enum';
 import { FontHeadSize } from '@libs/ui/enums/font-head-size';
+import { DOM } from '@libs/core/dom';
 
 export class Styles {
+    public static setDataColor(element: FrameworkElement, color: string) {
+        DOM.setAttribute(element, 'data-color', color);
+    }
+
     public static setFontFamily(element: FrameworkElement, fontFamily: FontFamily) {
         element.style.fontFamily = `var(${fontFamily})`;
     }
