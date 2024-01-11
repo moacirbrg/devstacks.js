f6c66535c770e5c3ba9c96b4acd3ad98a3dd977c
Moacir Braga <contato@moacirbrg.org>
2024-01-10 22:11:27 -0300

    Better and straightforward samples for buttons

--- src/components/controllers/samples/sample-form-button/sample-form-button.controller.tsx
+++ src/components/controllers/samples/sample-form-button/sample-form-button.controller.tsx
@@ -6,6 +6,8 @@ import { Color } from '@libs/ui/enums/color.enum';
 import { TypoHeading } from '@libs/ui/typo/heading/typo-heading.component';
 import { TypoHeadingType } from '@libs/ui/typo/heading/typo-heading-type';
 import { FontFamily } from '@libs/ui/enums/font-family.enum';
+import { FontSize } from '@libs/ui/enums/font-content-size.enum';
+import { FontWeight } from '@libs/ui/enums/font-weight.enum';
 
 export class SampleFormButtonController extends Component {
     public render(): FrameworkElement {
@@ -21,33 +23,21 @@ export class SampleFormButtonController extends Component {
                 <FormButton Color={Color.Warning}>Warning Button Color</FormButton>
 
                 <TypoHeading Type={TypoHeadingType.H2}>Font Family</TypoHeading>
-                <FormButton Color={Color.Unknown} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Unknown} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Unknown} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
-                <br/>
-                <FormButton Color={Color.Primary} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Primary} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Primary} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
-                <br/>
-                <FormButton Color={Color.Secondary} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Secondary} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Secondary} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
-                <br/>
-                <FormButton Color={Color.Danger} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Danger} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Danger} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
-                <br/>
-                <FormButton Color={Color.Info} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Info} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Info} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
-                <br/>
-                <FormButton Color={Color.Success} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Success} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Success} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
-                <br/>
-                <FormButton Color={Color.Warning} FontFamily={FontFamily.SansSerif}>Sans Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Warning} FontFamily={FontFamily.Serif}>Serif Font Family</FormButton>&nbsp;
-                <FormButton Color={Color.Warning} FontFamily={FontFamily.Monospace}>Monospace Font Family</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontFamily={FontFamily.SansSerif}>Sans-Serif</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontFamily={FontFamily.Serif}>Serif</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontFamily={FontFamily.Monospace}>Monospace</FormButton>&nbsp;
+
+                <TypoHeading Type={TypoHeadingType.H2}>Font Size</TypoHeading>
+                <FormButton Color={Color.Primary} FontSize={FontSize.ExtraSmall}>Extra Small</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontSize={FontSize.Small}>Small</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontSize={FontSize.Medium}>Default</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontSize={FontSize.Large}>Large</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontSize={FontSize.ExtraLarge}>Extra Large</FormButton>&nbsp;
+
+                <TypoHeading Type={TypoHeadingType.H2}>Font Weight</TypoHeading>
+                <FormButton Color={Color.Primary} FontWeight={FontWeight.Light}>Light</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontWeight={FontWeight.Regular}>Regular</FormButton>&nbsp;
+                <FormButton Color={Color.Primary} FontWeight={FontWeight.Bold}>Bold</FormButton>&nbsp;
             </div>
         );
     }
