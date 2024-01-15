7962738a6c7d58cd48773b18b70898f3bb16899c
Moacir Braga <contato@moacirbrg.org>
2024-01-15 13:31:01 -0300

    Customization of typo-message component only using SCSS

--- src/components/controllers/samples/sample-typo-message/sample-typo-message.controller.tsx
+++ src/components/controllers/samples/sample-typo-message/sample-typo-message.controller.tsx
@@ -2,75 +2,23 @@ import { FrameworkElement } from '@libs/core/framework-element';
 import { Component } from '@libs/core/component';
 import { DOM } from '@libs/core/dom';
 import { TypoHeading } from '@libs/ui/typo/heading/typo-heading.component';
-import { FontFamily } from '@libs/ui/enums/font-family.enum';
 import { TypoMessage } from '@libs/ui/typo/message/typo-message.component';
 import { Color } from '@libs/ui/enums/color.enum';
-import { FontWeight } from '@libs/ui/enums/font-weight.enum';
 import { TypoHeadingType } from '@libs/ui/typo/heading/typo-heading-type';
 
 export class SampleTypoMessageController extends Component {
     public render(): FrameworkElement {
         return (
             <div class="sample-typo-heading-controller">
-                <TypoHeading Type={TypoHeadingType.H2}>Light</TypoHeading>
-                <TypoMessage Color={Color.Unknown} FontWeight={FontWeight.Light}>Light Unknown Message</TypoMessage>
-                <TypoMessage Color={Color.Neutral} FontWeight={FontWeight.Light}>Light Neutral Message</TypoMessage>
-                <TypoMessage Color={Color.Primary} FontWeight={FontWeight.Light}>Light Primary Message</TypoMessage>
-                <TypoMessage Color={Color.Secondary} FontWeight={FontWeight.Light}>Light Secondary Message</TypoMessage>
-                <TypoMessage Color={Color.Danger} FontWeight={FontWeight.Light}>Light Danger Message</TypoMessage>
-                <TypoMessage Color={Color.Success} FontWeight={FontWeight.Light}>Light Success Message</TypoMessage>
-                <TypoMessage Color={Color.Warning} FontWeight={FontWeight.Light}>Light Warning Message</TypoMessage>
-                <TypoMessage Color={Color.Info} FontWeight={FontWeight.Light}>Light Info Message</TypoMessage>
-
-                <TypoHeading Type={TypoHeadingType.H2}>Regular</TypoHeading>
-                <TypoMessage Color={Color.Unknown} FontWeight={FontWeight.Regular}>Regular Unknown Message</TypoMessage>
-                <TypoMessage Color={Color.Neutral} FontWeight={FontWeight.Regular}>Regular Neutral Message</TypoMessage>
-                <TypoMessage Color={Color.Primary} FontWeight={FontWeight.Regular}>Regular Primary Message</TypoMessage>
-                <TypoMessage Color={Color.Secondary} FontWeight={FontWeight.Regular}>Regular Secondary Message</TypoMessage>
-                <TypoMessage Color={Color.Danger} FontWeight={FontWeight.Regular}>Regular Danger Message</TypoMessage>
-                <TypoMessage Color={Color.Success} FontWeight={FontWeight.Regular}>Regular Success Message</TypoMessage>
-                <TypoMessage Color={Color.Warning} FontWeight={FontWeight.Regular}>Regular Warning Message</TypoMessage>
-                <TypoMessage Color={Color.Info} FontWeight={FontWeight.Regular}>Regular Info Message</TypoMessage>
-
-                <TypoHeading Type={TypoHeadingType.H2}>Bold</TypoHeading>
-                <TypoMessage Color={Color.Unknown} FontWeight={FontWeight.Bold}>Bold Unknown Message</TypoMessage>
-                <TypoMessage Color={Color.Neutral} FontWeight={FontWeight.Bold}>Bold Neutral Message</TypoMessage>
-                <TypoMessage Color={Color.Primary} FontWeight={FontWeight.Bold}>Bold Primary Message</TypoMessage>
-                <TypoMessage Color={Color.Secondary} FontWeight={FontWeight.Bold}>Bold Secondary Message</TypoMessage>
-                <TypoMessage Color={Color.Danger} FontWeight={FontWeight.Bold}>Bold Danger Message</TypoMessage>
-                <TypoMessage Color={Color.Success} FontWeight={FontWeight.Bold}>Bold Success Message</TypoMessage>
-                <TypoMessage Color={Color.Warning} FontWeight={FontWeight.Bold}>Bold Warning Message</TypoMessage>
-                <TypoMessage Color={Color.Info} FontWeight={FontWeight.Bold}>Bold Info Message</TypoMessage>
-
-                <TypoHeading Type={TypoHeadingType.H2}>Sans-Serif</TypoHeading>
-                <TypoMessage Color={Color.Unknown}>Sans-Serif Unknown Message</TypoMessage>
-                <TypoMessage Color={Color.Neutral}>Sans-Serif Neutral Message</TypoMessage>
-                <TypoMessage Color={Color.Primary}>Sans-Serif Primary Message</TypoMessage>
-                <TypoMessage Color={Color.Secondary}>Sans-Serif Secondary Message</TypoMessage>
-                <TypoMessage Color={Color.Danger}>Sans-Serif Danger Message</TypoMessage>
-                <TypoMessage Color={Color.Success}>Sans-Serif Success Message</TypoMessage>
-                <TypoMessage Color={Color.Warning}>Sans-Serif Warning Message</TypoMessage>
-                <TypoMessage Color={Color.Info}>Sans-Serif Info Message</TypoMessage>
-
-                <TypoHeading Type={TypoHeadingType.H2}>Serif</TypoHeading>
-                <TypoMessage Color={Color.Unknown} FontFamily={FontFamily.Serif}>Serif Unknown Message</TypoMessage>
-                <TypoMessage Color={Color.Neutral} FontFamily={FontFamily.Serif}>Serif Neutral Message</TypoMessage>
-                <TypoMessage Color={Color.Primary} FontFamily={FontFamily.Serif}>Serif Primary Message</TypoMessage>
-                <TypoMessage Color={Color.Secondary} FontFamily={FontFamily.Serif}>Serif Secondary Message</TypoMessage>
-                <TypoMessage Color={Color.Danger} FontFamily={FontFamily.Serif}>Serif Danger Message</TypoMessage>
-                <TypoMessage Color={Color.Success} FontFamily={FontFamily.Serif}>Serif Success Message</TypoMessage>
-                <TypoMessage Color={Color.Warning} FontFamily={FontFamily.Serif}>Serif Warning Message</TypoMessage>
-                <TypoMessage Color={Color.Info} FontFamily={FontFamily.Serif}>Serif Info Message</TypoMessage>
-
-                <TypoHeading Type={TypoHeadingType.H2}>Monospace</TypoHeading>
-                <TypoMessage Color={Color.Unknown} FontFamily={FontFamily.Monospace}>Monospace Unknown Message</TypoMessage>
-                <TypoMessage Color={Color.Neutral} FontFamily={FontFamily.Monospace}>Monospace Neutral Message</TypoMessage>
-                <TypoMessage Color={Color.Primary} FontFamily={FontFamily.Monospace}>Monospace Primary Message</TypoMessage>
-                <TypoMessage Color={Color.Secondary} FontFamily={FontFamily.Monospace}>Monospace Secondary Message</TypoMessage>
-                <TypoMessage Color={Color.Danger} FontFamily={FontFamily.Monospace}>Monospace Danger Message</TypoMessage>
-                <TypoMessage Color={Color.Success} FontFamily={FontFamily.Monospace}>Monospace Success Message</TypoMessage>
-                <TypoMessage Color={Color.Warning} FontFamily={FontFamily.Monospace}>Monospace Warning Message</TypoMessage>
-                <TypoMessage Color={Color.Info} FontFamily={FontFamily.Monospace}>Monospace Info Message</TypoMessage>
+                <TypoHeading Type={TypoHeadingType.H2}>Single-line</TypoHeading>
+                <TypoMessage Color={Color.Unknown}>Message with Unknown color</TypoMessage>
+                <TypoMessage Color={Color.Neutral}>Message with Neutral color</TypoMessage>
+                <TypoMessage Color={Color.Primary}>Message with Primary color</TypoMessage>
+                <TypoMessage Color={Color.Secondary}>Message with Secondary color</TypoMessage>
+                <TypoMessage Color={Color.Danger}>Message with Danger color</TypoMessage>
+                <TypoMessage Color={Color.Success}>Message with Success color</TypoMessage>
+                <TypoMessage Color={Color.Warning}>Message with Warning color</TypoMessage>
+                <TypoMessage Color={Color.Info}>Message with Info color</TypoMessage>
 
                 <TypoHeading Type={TypoHeadingType.H2}>Multi-line</TypoHeading>
                 <TypoMessage Color={Color.Unknown}>
--- src/libs/ui/typo/message/typo-message.component.tsx
+++ src/libs/ui/typo/message/typo-message.component.tsx
@@ -1,20 +1,14 @@
+import "@styles/components/typo/typo-message.vars.scss"
 import "./typo-message.component.scss";
 import { Color } from "@libs/ui/enums/color.enum";
 import { FrameworkElement } from "@libs/core/framework-element";
 import { Component } from "@libs/core/component";
 import { ComponentProps } from "@libs/core/component-props";
 import { DOM } from '@libs/core/dom';
-import { FontFamily } from '@libs/ui/enums/font-family.enum';
-import { FontWeight } from '@libs/ui/enums/font-weight.enum';
-import { TypoMessageGlobals } from '@libs/ui/typo/message/typo-message.globals';
 import { Styles } from '@libs/ui/styles';
-import { FontSize } from '@libs/ui/enums/font-content-size.enum';
 
 export interface TypoMessageProps extends ComponentProps {
     Color?: Color;
-    FontFamily?: FontFamily;
-    FontSize?: FontSize;
-    FontWeight?: FontWeight;
 }
 
 export class TypoMessage extends Component<TypoMessageProps> {
@@ -25,32 +19,13 @@ export class TypoMessage extends Component<TypoMessageProps> {
             </div>
         );
 
-        Styles.setDataColor(element, this.props.Color || TypoMessageGlobals.Color);
-        Styles.setFontFamily(element, this.props.FontFamily || TypoMessageGlobals.FontFamily);
-        Styles.setFontSize(element, this.props.FontSize || TypoMessageGlobals.FontSize);
-        Styles.setFontWeight(element, this.props.FontWeight || TypoMessageGlobals.FontWeight);
-
+        Styles.setDataColor(element, this.props.Color || Color.Unknown);
         return element;
     }
 
     public setColor(color: Color): void {
         this.props.Color = color;
         Styles.setDataColor(this.element!, color);
-    }
-
-    public setFontFamily(fontFamily: FontFamily): void {
-        this.props.FontFamily = fontFamily;
-        Styles.setFontFamily(this.element!, fontFamily);
-    }
-
-    public setFontSize(fontSize: FontSize): void {
-        this.props.FontSize = fontSize;
-        Styles.setFontSize(this.element!, fontSize);
-    }
-
-    public setFontWeight(fontWeight: FontWeight): void {
-        this.props.FontWeight = fontWeight;
-        Styles.setFontWeight(this.element!, fontWeight);
     }
 
     public setContent(content: string|FrameworkElement): void {
