b2f885af8f46cd43cbd327c8cb4282903773b637
Moacir Braga <contato@moacirbrg.org>
2024-01-15 12:48:55 -0300

    Customization of typo-heading component only using SCSS

--- src/components/controllers/samples/sample-typo-heading/sample-typo-heading.controller.tsx
+++ src/components/controllers/samples/sample-typo-heading/sample-typo-heading.controller.tsx
@@ -2,8 +2,6 @@ import { FrameworkElement } from '@libs/core/framework-element';
 import { Component } from '@libs/core/component';
 import { DOM } from '@libs/core/dom';
 import { TypoHeading } from '@libs/ui/typo/heading/typo-heading.component';
-import { FontWeight } from '@libs/ui/enums/font-weight.enum';
-import { FontFamily } from '@libs/ui/enums/font-family.enum';
 import { TypoHeadingType } from '@libs/ui/typo/heading/typo-heading-type';
 import { Color } from '@libs/ui/enums/color.enum';
 
@@ -11,94 +9,53 @@ export class SampleTypoHeadingController extends Component {
     public render(): FrameworkElement {
         return (
             <div class="sample-typo-heading-controller">
-                <TypoHeading Type={TypoHeadingType.H1} FontWeight={FontWeight.Light}> My Title H1 Sans-Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontWeight={FontWeight.Light}> My Title H2 Sans-Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Light}> My Title H3 Sans-Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontWeight={FontWeight.Light}> My Title H4 Sans-Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontWeight={FontWeight.Light}> My Title H5 Sans-Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontWeight={FontWeight.Light}> My Title H6 Sans-Serif Light</TypoHeading>
-
+                <TypoHeading Type={TypoHeadingType.H1}>My Title H1 - Unknown Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H2}>My Title H2 - Unknown Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H3}>My Title H3 - Unknown Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H4}>My Title H4 - Unknown Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H5}>My Title H5 - Unknown Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H6}>My Title H6 - Unknown Color</TypoHeading>
                 <hr/>
 
-                <TypoHeading Type={TypoHeadingType.H1} FontWeight={FontWeight.Regular}> My Title H1 Sans-Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontWeight={FontWeight.Regular}> My Title H2 Sans-Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular}> My Title H3 Sans-Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontWeight={FontWeight.Regular}> My Title H4 Sans-Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontWeight={FontWeight.Regular}> My Title H5 Sans-Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontWeight={FontWeight.Regular}> My Title H6 Sans-Serif Regular</TypoHeading>
-
+                <TypoHeading Type={TypoHeadingType.H1} Color={Color.Primary}>My Title H1 - Primary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H2} Color={Color.Primary}>My Title H2 - Primary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H3} Color={Color.Primary}>My Title H3 - Primary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H4} Color={Color.Primary}>My Title H4 - Primary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H5} Color={Color.Primary}>My Title H5 - Primary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H6} Color={Color.Primary}>My Title H6 - Primary Color</TypoHeading>
                 <hr/>
 
-                <TypoHeading Type={TypoHeadingType.H1}> My Title H1 Sans-Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2}> My Title H2 Sans-Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3}> My Title H3 Sans-Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4}> My Title H4 Sans-Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5}> My Title H5 Sans-Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6}> My Title H6 Sans-Serif Bold</TypoHeading>
-
+                <TypoHeading Type={TypoHeadingType.H1} Color={Color.Secondary}>My Title H1 - Secondary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H2} Color={Color.Secondary}>My Title H2 - Secondary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H3} Color={Color.Secondary}>My Title H3 - Secondary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H4} Color={Color.Secondary}>My Title H4 - Secondary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H5} Color={Color.Secondary}>My Title H5 - Secondary Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H6} Color={Color.Secondary}>My Title H6 - Secondary Color</TypoHeading>
                 <hr/>
 
-                <TypoHeading Type={TypoHeadingType.H1} FontWeight={FontWeight.Light} FontFamily={FontFamily.Serif}> My Title H1 Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontWeight={FontWeight.Light} FontFamily={FontFamily.Serif}> My Title H2 Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Light} FontFamily={FontFamily.Serif}> My Title H3 Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontWeight={FontWeight.Light} FontFamily={FontFamily.Serif}> My Title H4 Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontWeight={FontWeight.Light} FontFamily={FontFamily.Serif}> My Title H5 Serif Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontWeight={FontWeight.Light} FontFamily={FontFamily.Serif}> My Title H6 Serif Light</TypoHeading>
-
+                <TypoHeading Type={TypoHeadingType.H1} Color={Color.Success}>My Title H1 - Success Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H2} Color={Color.Success}>My Title H2 - Success Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H3} Color={Color.Success}>My Title H3 - Success Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H4} Color={Color.Success}>My Title H4 - Success Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H5} Color={Color.Success}>My Title H5 - Success Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H6} Color={Color.Success}>My Title H6 - Success Color</TypoHeading>
                 <hr/>
 
-                <TypoHeading Type={TypoHeadingType.H1} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Serif}> My Title H1 Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Serif}> My Title H2 Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Serif}> My Title H3 Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Serif}> My Title H4 Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Serif}> My Title H5 Serif Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Serif}> My Title H6 Serif Regular</TypoHeading>
-
+                <TypoHeading Type={TypoHeadingType.H1} Color={Color.Danger}>My Title H1 - Danger Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H2} Color={Color.Danger}>My Title H2 - Danger Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H3} Color={Color.Danger}>My Title H3 - Danger Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H4} Color={Color.Danger}>My Title H4 - Danger Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H5} Color={Color.Danger}>My Title H5 - Danger Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H6} Color={Color.Danger}>My Title H6 - Danger Color</TypoHeading>
                 <hr/>
 
-                <TypoHeading Type={TypoHeadingType.H1} FontFamily={FontFamily.Serif}> My Title H1 Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontFamily={FontFamily.Serif}> My Title H2 Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontFamily={FontFamily.Serif}> My Title H3 Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontFamily={FontFamily.Serif}> My Title H4 Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontFamily={FontFamily.Serif}> My Title H5 Serif Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontFamily={FontFamily.Serif}> My Title H6 Serif Bold</TypoHeading>
-
+                <TypoHeading Type={TypoHeadingType.H1} Color={Color.Warning}>My Title H1 - Warning Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H2} Color={Color.Warning}>My Title H2 - Warning Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H3} Color={Color.Warning}>My Title H3 - Warning Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H4} Color={Color.Warning}>My Title H4 - Warning Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H5} Color={Color.Warning}>My Title H5 - Warning Color</TypoHeading>
+                <TypoHeading Type={TypoHeadingType.H6} Color={Color.Warning}>My Title H6 - Warning Color</TypoHeading>
                 <hr/>
-
-                <TypoHeading Type={TypoHeadingType.H1} FontWeight={FontWeight.Light} FontFamily={FontFamily.Monospace}> My Title H1 Monospace Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontWeight={FontWeight.Light} FontFamily={FontFamily.Monospace}> My Title H2 Monospace Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Light} FontFamily={FontFamily.Monospace}> My Title H3 Monospace Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontWeight={FontWeight.Light} FontFamily={FontFamily.Monospace}> My Title H4 Monospace Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontWeight={FontWeight.Light} FontFamily={FontFamily.Monospace}> My Title H5 Monospace Light</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontWeight={FontWeight.Light} FontFamily={FontFamily.Monospace}> My Title H6 Monospace Light</TypoHeading>
-
-                <hr/>
-
-                <TypoHeading Type={TypoHeadingType.H1} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Monospace}> My Title H1 Monospace Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Monospace}> My Title H2 Monospace Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Monospace}> My Title H3 Monospace Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Monospace}> My Title H4 Monospace Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Monospace}> My Title H5 Monospace Regular</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontWeight={FontWeight.Regular} FontFamily={FontFamily.Monospace}> My Title H6 Monospace Regular</TypoHeading>
-
-                <hr/>
-
-                <TypoHeading Type={TypoHeadingType.H1} FontFamily={FontFamily.Monospace}> My Title H1 Monospace Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H2} FontFamily={FontFamily.Monospace}> My Title H2 Monospace Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontFamily={FontFamily.Monospace}> My Title H3 Monospace Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H4} FontFamily={FontFamily.Monospace}> My Title H4 Monospace Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H5} FontFamily={FontFamily.Monospace}> My Title H5 Monospace Bold</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H6} FontFamily={FontFamily.Monospace}> My Title H6 Monospace Bold</TypoHeading>
-
-                <hr/>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Unknown}> My Title Unknown</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Neutral}> My Title Neutral</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Primary}> My Title Primary</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Secondary}> My Title Secondary</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Danger}> My Title Danger</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Info}> My Title Info</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Success}> My Title Success</TypoHeading>
-                <TypoHeading Type={TypoHeadingType.H3} FontWeight={FontWeight.Regular} Color={Color.Warning}> My Title Warning</TypoHeading>
             </div>
         );
     }
--- src/libs/ui/typo/heading/typo-heading.component.tsx
+++ src/libs/ui/typo/heading/typo-heading.component.tsx
@@ -1,21 +1,16 @@
+import "@styles/components/typo/typo-heading.vars.scss";
 import "./typo-heading.component.scss";
 import { FrameworkElement } from "@libs/core/framework-element";
 import { Component } from "@libs/core/component";
 import { ComponentProps } from "@libs/core/component-props";
 import { DOM } from '@libs/core/dom';
-import { TypoHeadingGlobals } from '@libs/ui/typo/heading/typo-heading.globals';
-import { FontWeight } from '@libs/ui/enums/font-weight.enum';
-import { FontFamily } from '@libs/ui/enums/font-family.enum';
 import { Styles } from '@libs/ui/styles';
 import { TypoHeadingType } from '@libs/ui/typo/heading/typo-heading-type';
 import { Color } from "@libs/ui/enums/color.enum";
 
 export interface TypoHeadingProps extends ComponentProps {
-    Type: TypoHeadingType;
-
     Color?: Color;
-    FontWeight?: FontWeight;
-    FontFamily?: FontFamily;
+    Type: TypoHeadingType;
 }
 
 export class TypoHeading extends Component<TypoHeadingProps> {
@@ -26,10 +21,8 @@ export class TypoHeading extends Component<TypoHeadingProps> {
             </div>
         );
 
-        Styles.setDataColor(element, this.props.Color || TypoHeadingGlobals.Color);
-        Styles.setFontWeight(element, this.props.FontWeight || TypoHeadingGlobals.getWeight(this.props.Type));
+        Styles.setDataColor(element, this.props.Color || Color.Unknown);
         this._setType(element, this.props.Type);
-        Styles.setFontFamily(element, this.props.FontFamily || TypoHeadingGlobals.FontFamily);
 
         return element;
     }
@@ -39,13 +32,9 @@ export class TypoHeading extends Component<TypoHeadingProps> {
         Styles.setDataColor(this.element!, color);
     }
 
-    public setFontFamily(fontFamily: FontFamily): void {
-        this.props.FontFamily = fontFamily;
-        Styles.setFontFamily(this.element!, fontFamily);
-    }
-
-    private _setType(element: FrameworkElement, type: TypoHeadingType): void {
-        DOM.setAttribute(element, 'data-type', type);
+    public setContent(content: string|FrameworkElement): void {
+        this.children = [content];
+        DOM.replaceChildren(this.element!, ...this.children as string[]|FrameworkElement[]);
     }
 
     public setType(size: TypoHeadingType): void {
@@ -53,13 +42,7 @@ export class TypoHeading extends Component<TypoHeadingProps> {
         this._setType(this.element!, size);
     }
 
-    public setFontWeight(weight: FontWeight): void {
-        this.props.FontWeight = weight;
-        Styles.setFontWeight(this.element!, weight);
-    }
-
-    public setContent(content: string|FrameworkElement): void {
-        this.children = [content];
-        DOM.replaceChildren(this.element!, ...this.children as string[]|FrameworkElement[]);
+    private _setType(element: FrameworkElement, type: TypoHeadingType): void {
+        DOM.setAttribute(element, 'data-type', type);
     }
 }
